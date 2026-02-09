c561265ac3533e8d7afd44b12c1db8ac1a4adf79
ilyasmadoui <ilyasmadoui2020@gmail.com>
2026-02-09 19:24:51 +0100

    Delete oppenoffers_Frontend/Frontend directory

--- oppenoffers_Frontend/Frontend/src/hooks/useToast.js
+++
@@ -1,9 +0,0 @@
-import { toast } from 'react-toastify';
-
-export const useToast = () => {
-  const showToast = (message, type = 'default') => {
-    toast(message, { type, position: 'top-right' });
-  };
-
-  return { showToast };
-};
